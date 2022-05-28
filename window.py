fdc5bab58987e24305f8f86d3c9800cc730acf31
Dhiman Ghosh <dgkiitcsedual@gmail.com>
2022-05-29 03:35:51 +0530

    Update window.py

--- window.py
+++ window.py
@@ -17,9 +17,9 @@ class Window(QWidget):
         # Canvas
         stylesheet = ('''
             QWidget {
-                font-size: 18px;
+                font-size: 12px;
             }
-
+            
             QPushButton {
                 font-size: 20px;
             }
@@ -34,15 +34,18 @@ class Window(QWidget):
         label = QLabel('Youtube video URL')
         self.inputField = QLineEdit()
         button = QPushButton('Download')
+        self.status_label = QLabel()
 
         layout.addWidget(label)
         layout.addWidget(self.inputField)
         layout.addWidget(button)
+        layout.addWidget(self.status_label)
 
         button.clicked.connect(self.start_download)
 
     def start_download(self):
         url = self.inputField.text()
         yt = YouTube(url)
+        self.status_label.setText('Downloading video... Please Wait!')
         yt.streams.get_highest_resolution().download()
-        self.status_label.setText('Download completed!')
+        self.status_label.setText(f'Download completed!')
